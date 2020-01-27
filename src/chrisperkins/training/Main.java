98004378dc05b801a55ddc7d30147445e90bb326
CPERKINS <chrisperkins.cap@gmail.com>
2020-01-27 12:19:48 +0100

    Changes myMinValue to myMinIntValue

--- src/chrisperkins/training/Main.java
+++ src/chrisperkins/training/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
         int myValue = 10000;
 
-        int myMinValue = Integer.MIN_VALUE;
-        int myMaxValue = Integer.MAX_VALUE;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
 
-        System.out.println("Integer Minimum Value = " + myMinValue );
-        System.out.println("Integer Maximum Value = " + myMaxValue );
+        System.out.println("Integer Minimum Value = " + myMinIntValue );
+        System.out.println("Integer Maximum Value = " + myMaxIntValue );
 
-        System.out.println("Busted Max Value = " + (myMaxValue + 1));
-        System.out.println("Busted Max Value = " + (myMinValue - 1));
+        System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Max Value = " + (myMinIntValue - 1));
 
         int myMaxIntTest = 2_147_483_647;
 
